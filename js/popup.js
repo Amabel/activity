0402b0abf9c8aa96d7726319f804935a0a53e064
Amabel <luoweibinb@gmail.com>
2018-07-23 21:44:38 +0900

    feat(popup): show verifying div

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ GITHUB_API_ENDPOINT = 'https://api.github.com';
 $('#token-submit').validator()
 
 $('#token-submit').submit(function() {
+  showVerifyingDiv();
   let data = $(this).serializeArray()
   let accessToken = data[0]['value'];
   console.log('form submitted, token: ' + accessToken);
@@ -18,9 +19,29 @@ function validateAccessToken(accessToken) {
     dataType: 'json',
     success: function(data) {
       console.log(JSON.stringify(data));
+      showMainContainer();
     },
     error: function(error) {
+      showTokenSubmit();
       console.log(JSON.stringify(error));
     }
   })
+}
+
+function showTokenSubmit() {
+  $('.verifying-token').hide();
+  $('.main-container').hide();
+  $('#token-submit').show();
+}
+
+function showVerifyingDiv() {
+  $('#token-submit').hide();
+  $('.main-container').hide();
+  $('.verifying-token').show();
+}
+
+function showMainContainer() {
+  $('#token-submit').hide();
+  $('.verifying-token').hide();
+  $('.main-container').show();
 }
