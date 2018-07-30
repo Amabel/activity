95419c22947a404a5ff8e4851602ed4fcd5f2b7e
Amabel <luoweibinb@gmail.com>
2018-07-30 22:43:11 +0900

    feat(popup): show popup in all urls

--- js/background.js
+++ js/background.js
@@ -7,17 +7,17 @@
 // Global variables
 // GITHUB_API_ENDPOINT = newFunction();
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'github.com'},
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//     chrome.declarativeContent.onPageChanged.addRules([{
+//       conditions: [new chrome.declarativeContent.PageStateMatcher({
+//         pageUrl: {hostEquals: 'github.com'},
+//       })
+//       ],
+//       actions: [new chrome.declarativeContent.ShowPageAction()]
+//     }]);
+//   });
+// });
 
 function sendMessageToContentScript(message, callback)
 {
