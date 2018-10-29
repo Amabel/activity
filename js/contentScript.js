9eaeec166beee74aefec9e2a41afbfca8443fec5
Amabel <luoweibinb@gmail.com>
2018-10-29 16:16:48 +0900

    feat: add support for wacth event

--- js/contentScript.js
+++ js/contentScript.js
@@ -194,6 +194,9 @@ function resolveActivity(activity) {
     case 'ReleaseEvent':
       contentDiv += getReleaseEventTypeContent(activity);
       break;
+    case 'WatchEvent':
+      contentDiv += getWatchEventTypeContent(activity);
+      break;
     default:
       unsupportedActivityNum ++;
       console.log('unsupported type: ' + activityType);
@@ -592,6 +595,41 @@ function getReleaseEventTypeContent(activity) {
                       '<div class="action-description">' +
                         '<a href="' + userUrl + '" class="username">' + username + '</a>' +
                         ' published a ' + preRelease + 'release ' + '<a class="ga-bold" href="' + actionUrl + '" target="_blank">' + releaseName + '</a>' + ' in <a href="' + repoUrl + '" target="_blank">' + repoName + '</a>' +
+                      '</div>' +
+                    '</div>' +
+                    '<div class="time-stamp">' +
+                      timeFromNow
+                    '</div>' +
+                  '</div>'
+                '</div>' +
+              '</div>';
+  return contents;
+}
+
+function getWatchEventTypeContent(activity) {
+  let contents = '';
+  let username = activity.actor.login;
+  let userUrl = GITHUB_PREFIX + username;
+  let avatarUrl = activity.actor.avatar_url;
+  let orgRepoName = activity.repo.name;
+  let repoName = activity.repo.name.split('/')[1];
+  let repoUrl = 'https://github.com/' + orgRepoName;
+  let createdAt = activity.created_at;
+  let timeFromNow = moment(createdAt).fromNow();
+  let iconUrl = chrome.runtime.getURL('images/icons/eye.svg');
+  contents += '<div class="activity-content-wrapper">' +
+                '<div class="activity-row">' +
+                  '<div class="activity-icon-wrapper">' +
+                    '<img src="' + iconUrl + '">' +
+                  '</div>' +
+                  '<div class="activity-description">' +
+                    '<div class="action">' +
+                      '<div class="ga-avatar">' +
+                        '<img src="' + avatarUrl + '">' +
+                      '</div>' +
+                      '<div class="action-description">' +
+                        '<a href="' + userUrl + '" class="username">' + username + '</a>' +
+                        ' started watching <a href="' + repoUrl + '" target="_blank">' + repoName + '</a>' +
                       '</div>' +
                     '</div>' +
                     '<div class="time-stamp">' +
